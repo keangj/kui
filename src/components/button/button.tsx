8a6bc1a81c758a2c9067871bb5de8d9183b23220
jay <keangjay@gmail.com>
2021-08-18 15:39:41 +0800

    feat(button): 完成基本功能

---
+++ src/components/button/button.tsx
@@ -0,0 +1,53 @@
+import { computed, defineComponent, ExtractPropTypes, PropType } from 'vue'
+import './button.scss'
+
+type IButtonType = PropType<
+  'default' | 'primary' | 'info' | 'success' | 'warning' | 'error'
+>
+type IButtonSize = PropType<'large' | 'medium' | 'small' | 'mini'>
+
+const buttonProps = {
+  type: {
+    type: String as IButtonType,
+    default: 'default'
+  },
+  loading: Boolean,
+  disabled: Boolean,
+  size: String as IButtonSize,
+  onClick: Function as unknown as () => (event: MouseEvent) => void
+}
+
+export type ButtonProps = ExtractPropTypes<typeof buttonProps>
+
+const Button = defineComponent({
+  name: 'Button',
+  props: buttonProps,
+  // emits: ['click'],
+  setup (props, { emit }) {
+    const handleClick = (event: MouseEvent) => {
+      emit('click', event)
+      // const { onClick } = props
+      // onClick?.(event)
+    }
+    const classes = computed(() => {
+      return [
+        'kui-button',
+        `kui-button--${props.type}`,
+        props.size && `kui-button--${props.size}`
+      ]
+    })
+    return { classes, handleClick }
+  },
+  render () {
+    const { $slots, classes, disabled } = this
+    return (
+      <>
+        <button class={classes} disabled={disabled} onClick={this.handleClick}>
+          {$slots.default?.()}
+        </button>
+      </>
+    )
+  }
+})
+
+export default Button
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,15 +1,40 @@
 <script lang="tsx">
 import { defineComponent } from 'vue'
-import Button from '../components/button'
+import Button from '../components/button/button'
 export default defineComponent({
   name: 'Home',
   components: {
     Button
   },
-  render: () => (
-    <>
-      <Button>提交</Button>
-    </>
-  )
+  setup () {
+    const onClick = () => {
+      console.log(123)
+    }
+    return { onClick }
+  },
+  render () {
+    const { onClick } = this
+    return (
+      <>
+        <Button onClick={onClick}>提交</Button>
+        <Button disabled>提交</Button>
+        <Button type="error" disabled>
+          提交
+        </Button>
+        <Button type="warning" size="large" disabled>
+          提交
+        </Button>
+        <Button type="primary" size="medium">
+          提交
+        </Button>
+        <Button type="info" size="small">
+          提交
+        </Button>
+        <Button type="success" size="mini">
+          提交
+        </Button>
+      </>
+    )
+  }
 })
 </script>
